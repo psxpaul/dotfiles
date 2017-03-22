af82ff34086ac094464929431c510f4f0febd1ed
Paul Roberts <psxpaul@gmail.com>
2017-03-21 17:00:03 -0700

    Fixing typo in install script, and adding gradle script to find the closest gradlew instance

--- install.sh
+++ install.sh
@@ -40,18 +40,19 @@ for file in $dir/bash_completion.d/*; do
     ln -fs $file $HOME/.bash_completion.d/$base
 done
 
-echo "Creating symlink to .gitignore in home directory"
-ln -fs $dir/.gitignore $HOME/.gitignore
-
-echo "Creating symlink to meldGit in ~/bin"
-ln -fs $dir/meldGit $HOME/bin/meldGit
-ln -fs $dir/pretty $HOME/bin/pretty
-
 echo "Creating symlink to .profile in home directory"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     ln -fs $dir/profile $HOME/.bash_profile
-elif
+else
     ln -fs $dir/profile $HOME/.profile
 fi
+
+echo "Creating symlink to .gitignore in home directory"
+ln -fs $dir/.gitignore $HOME/.gitignore
+
+echo "Creating symlinks in ~/bin"
+ln -fs $dir/gradle $HOME/bin/gradle
+ln -fs $dir/meldGit $HOME/bin/meldGit
+ln -fs $dir/pretty $HOME/bin/pretty
 
 echo "Done!"
